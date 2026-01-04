29e3c8e0d86ad107ff5eb469474192ee4e5fd04e
KAnggara75 <KAnggara75@gmail.com>
2026-01-04 14:51:02 +0700

    init

---
+++ CoPas/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoPas
+//
+//  Created by Call  Vin on 04/01/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
